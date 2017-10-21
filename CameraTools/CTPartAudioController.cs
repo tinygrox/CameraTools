09fab7947eca07407f1b564fc51e7830021563ce
Jesus Rodriguez Valencia <jesus.rodriguez.valencia@gmail.com>
2017-10-21 18:56:50 +0200

    removing unnecesary usings

--- CameraTools/CTPartAudioController.cs
+++ CameraTools/CTPartAudioController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 namespace CameraTools
 {
 	public class CTPartAudioController : MonoBehaviour
--- CameraTools/CTPersistantFIeld.cs
+++ CameraTools/CTPersistantFIeld.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Reflection;
+
 namespace CameraTools
 {
 	[AttributeUsage(AttributeTargets.Field)]
