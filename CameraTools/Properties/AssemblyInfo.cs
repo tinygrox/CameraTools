0dfa7406ee7f7e410bc3b8bfa35d336a9f5e227f
Brett Ryland <b.n.ryland@gmail.com>
2021-09-24 18:15:58 +0200

    Update changelog. Bump version number.

--- CameraTools/Properties/AssemblyInfo.cs
+++ CameraTools/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "1.18.2.0" )]
-[assembly: AssemblyFileVersion( "1.18.2" )]
+[assembly: AssemblyVersion( "1.19.0.0" )]
+[assembly: AssemblyFileVersion( "1.19.0" )]
